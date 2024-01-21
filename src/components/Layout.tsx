f3cd4b45c9d4b887a41e4c5394d76671c1e22c75
keisuke-333 <keikubo.999@gmail.com>
2024-01-21 20:17:30 +0900

    add margin

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react"
 
 export const Layout = ({ children }: PropsWithChildren) => {
-  return <div className="flex flex-col items-center my-20">{children}</div>
+  return <div className="flex flex-col items-center my-20 mx-4">{children}</div>
 }
